7d24173084bcf2073dd7bfe51030e8ef6c101548
marson parulian <marson.parulian@gmail.com>
2021-09-08 23:30:39 +1000

    IRecord type definition

---
+++ src/types/dataSource.d.ts
@@ -0,0 +1,7 @@
+// This file contains type definitions related to data source response
+export interface IRecord {
+    notification_date: string,
+    cases: number,
+    lga_code19: string,
+    lg_name19: string
+}
