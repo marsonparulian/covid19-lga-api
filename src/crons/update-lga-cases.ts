cafa955b906453b48410ccfa69ef29cdac354d42
marson parulian <marson.parulian@gmail.com>
2021-09-08 23:31:32 +1000

    create method to fetch records from data source

--- src/crons/update-lga-cases.ts
+++ src/crons/update-lga-cases.ts
@@ -1,9 +1,13 @@
+import fetch from "cross-fetch";
+import { IRecord } from "../types/dataSource";
+
 /**
  * Update cases in each lga, based on the fetched data
  */
 export default async () => {
     try {
-        // TODO Fetch data from data source. Will throw error if response is error or invalid data format.
+        // Fetch data from data source. Will throw error if response is error or invalid data format.
+
 
         // TODO Process data to match DB schema .
 
@@ -11,4 +15,31 @@ export default async () => {
     } catch (e) {
         throw (e);
     }
+}
+export const fetchRecords = async (): Promise<IRecord[]> => {
+    // Prepare variables
+    const resourceId = "21304414-1ff1-4243-a5d2-f52778048b29"; // Resource for covid locations data.
+    const select = "notification_date, count(*) as cases, lga_code19, lga_name19 "; // Fields to be selected
+    const groupBy = "notification_date, lga_code19, lga_name19"; // Grouping fields
+    const orderBy = "notification_date";
+    const startDate = '2021-09-05'; // Start date of records  to be fetched.
+
+    //  Prepare sql statements
+    const sql = `select ${select} from "${resourceId}" where notification_date >  '${startDate}' group by ${groupBy} order by ${orderBy}`;
+    const url = `https://data.nsw.gov.au/data/api/3/action/datastore_search_sql?sql=${sql}`;
+
+    // Fetch
+    return fetch(url)
+        .then((response) => {
+            // Throw error if response not `ok`
+            if (!response.ok) {
+                throw new Error(JSON.stringify(response));
+            }
+            return response.json();
+        }).then((data) => {
+            if (!data.success) {
+                throw new Error("response.body.success is falsy");
+            }
+            return data.result.records;
+        });
 }
--- test/integrations/crons/update-lga-cases.test.ts
+++ test/integrations/crons/update-lga-cases.test.ts
@@ -1,6 +1,33 @@
+import updateLgaCases, { fetchRecords } from "../../../src/crons/update-lga-cases";
+
+// Increase timeout since one of test will require to fetch from data source.
+jest.setTimeout(12000);
 
 // Test updating LGA cases
 describe("Update LGA cases", () => {
     test.todo("Data source response with failed (unsuccessful) result");
-    test.todo("Dta source response with available data /records");
+    test.todo("Data source response with available data /records");
+    test("fetchRecords ", async () => {
+        const records = await fetchRecords().catch((e) => {
+            throw (e);
+        });
+
+        // Verify records has element more than 1
+        expect(records.length).toBeGreaterThan(0);
+        // Verify the sape of the first element match the `IRecord` interface
+        const shapeIRecord = expect.objectContaining({
+            cases: expect.any(Number),
+            notification_date: expect.any(String),
+            lga_code19: expect.any(String),
+            lga_name19: expect.any(String),
+        });
+        expect(records[0]).toEqual(shapeIRecord);
+        // Verify the shape of all elements
+        expect(records).toEqual(expect.arrayContaining([{
+            cases: expect.any(Number),
+            notification_date: expect.any(String),
+            lga_code19: expect.any(String),
+            lga_name19: expect.any(String),
+        }]));
+    });
 })
