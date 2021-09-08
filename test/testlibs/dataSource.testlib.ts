2747d1fc16a3513e2ffe0cd2fefd8108fce8ed79
marson parulian <marson.parulian@gmail.com>
2021-09-09 08:59:02 +1000

    init testlib to imitate 'records' from data source

---
+++ test/testlibs/dataSource.testlib.ts
@@ -0,0 +1,20 @@
+// This file contain test library to represent data from data source
+
+import { IRecord } from "../../src/types/dataSource";
+
+// Imitate `records` contained in `Response.body.result.records`
+export const record0  : IRecord = { notification_date: "2021-09-06", cases: 9, lga_code19: "X999", lga_name19: "Correctional settings" };
+export const record1  : IRecord = { notification_date: "2021-09-06", cases: 57, lga_code19: "16260", lga_name19: "Parramatta (C)" };
+export const record2  : IRecord = { notification_date: "2021-09-06", cases: 7, lga_code19: "18050", lga_name19: "Waverley (A)" };
+export const record3  : IRecord = { notification_date: "2021-09-06", cases: 1, lga_code19: "17900", lga_name19: "Walgett (A)" };
+export const record4  : IRecord = { notification_date: "2021-09-06", cases: 121, lga_code19: "10750", lga_name19: "Blacktown (C)" };
+export const record5  : IRecord = { notification_date: "2021-09-06", cases: 105, lga_code19: "14900", lga_name19: "Liverpool (C)" };
+export const record6  : IRecord = { notification_date: "2021-09-06", cases: 4, lga_code19: "14500", lga_name19: "Ku-ring-gai (A)" };
+export const record7  : IRecord = { notification_date: "2021-09-06", cases: 2, lga_code19: "14700", lga_name19: "Lane Cove (A)" };
+export const record8  : IRecord = { notification_date: "2021-09-06", cases: 1, lga_code19: "15240", lga_name19: "Mid-Coast (A)" };
+export const record9  : IRecord = { notification_date: "2021-09-06", cases: 271, lga_code19: "11570", lga_name19: "Canterbury-Bankstown (A)" };
+export const record10  : IRecord = { notification_date: "2021-09-06", cases: 21, lga_code19: "11650", lga_name19: "Central Coast (C) (NSW)" };
+export const record11  : IRecord = { notification_date: "2021-09-06", cases: 64, lga_code19: "12850", lga_name19: "Fairfield (C)" };
+export const record12  : IRecord = { notification_date: "2021-09-06", cases: 4, lga_code19: "16900", lga_name19: "Shellharbour (C)" };
+export const record13  : IRecord = { notification_date: "2021-09-06", cases: 12, lga_code19: "13800", lga_name19: "Hawkesbury (C)" };
+export const record14  : IRecord = { notification_date: "2021-09-06", cases: 1, lga_code19: "18400", lga_name19: "Wollondilly (A)" };
