87410f327a75c5ed21e8449aca6cdc8a222545e1
marson parulian <marson.parulian@gmail.com>
2021-09-08 12:54:39 +1000

    init test file / todo tests

---
+++ test/integrations/crons/update-lga-cases.test.ts
@@ -0,0 +1,6 @@
+
+// Test updating LGA cases
+describe("Update LGA cases", () => {
+    test.todo("Data source response with failed (unsuccessful) result");
+    test.todo("Dta source response with available data /records");
+})
