6c4879e1ae31c47842253b82385a0622ab21ea93
marson parulian <marson.parulian@gmail.com>
2021-09-11 07:25:45 +1000

    remove duplicate 'todo' test

--- test/integrations/crons/update-lga-cases.test.ts
+++ test/integrations/crons/update-lga-cases.test.ts
@@ -8,9 +8,8 @@ jest.setTimeout(12000);
 // Test updating LGA cases
 describe("Update LGA cases", () => {
     test.todo("Data source response with failed (unsuccessful) result");
-    test.todo("Data source response with available data /records");
     /*
-        test("fetchRecords ", async () => {
+        test("fetchRecords - successful", async () => {
             const records = await fetchRecords().catch((e) => {
                 throw (e);
             });
