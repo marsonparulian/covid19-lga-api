7b910abf9025441e39ee92c6d753d966cf98c8fc
marson parulian <marson.parulian@gmail.com>
2021-09-16 22:40:07 +1000

    init test scenario for GET '/lgas'

---
+++ test/routes/lgas.test.ts
@@ -0,0 +1,20 @@
+
+// Test for `/lgas` URL
+describe("GET /lgas ", () => {
+    test("Should return the saved LGAs", async () => {
+        // Drop collection
+
+        // Prepare LGAs to be saved 
+
+        // Save LGAs
+
+        // Fetch the newly inserted LGAs
+
+        // Should saved in DB
+
+        // Make request
+
+        // Response status should be 200
+        // Response body should contain the basic data of LGAs (without `notifiedCasesByDates`)
+    });
+});
